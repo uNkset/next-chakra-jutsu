575b651045f4c4925595dde9dcfb4f9f436ec186
Konstantin Arescenko <kostylo@windowslive.com>
2023-05-07 10:15:42 +0200

    added desktop navigation v.1

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,233 @@
-import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  useColorModeValue,
+  IconButton,
+  useDisclosure,
+  useBreakpointValue,
+  Text,
+  Stack,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Icon,
+  Button,
+  Link,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 export default function NavBar() {
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <Box>
-      <Flex bg={useColorModeValue('white', 'gray.800')}>
-        <h2>sup niggas</h2>
+      <Flex
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'60px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        align={'center'}
+      >
+        <Flex
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+          display={{ base: 'flex', md: 'none' }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Text
+            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+            fontFamily={'heading'}
+            color={useColorModeValue('gray.800', 'white')}
+          >
+            Logo
+          </Text>
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}
+        >
+          <Button
+            as={'a'}
+            fontSize={'sm'}
+            fontWeight={400}
+            variant={'link'}
+            href={'#'}
+          >
+            {' '}
+            Sign In
+          </Button>
+          <Button
+            as={'a'}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'pink.400'}
+            href={'#'}
+            _hover={{ bg: 'pink.300' }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
       </Flex>
     </Box>
   );
 }
+// useColorModeValue - color mode for light and dark theme
+// flex: 1 - the width of all other elements will be the same as their content, but the element with flex: 1 will be given the remaining full space.
+// ml - margin left
+//md - screen min width 48em
+//variant - different visual styling
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popBgColor = useColorModeValue('white', 'gray.800');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{ textDecoration: 'none', color: linkHoverColor }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+// Stack - layout component used to group elements together and apply a space between them.
+// p - padding
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+  return (
+    <Link
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
+    >
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}
+        >
+          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+// interface - specifies the structure of data. Another way is using type alias, less recommended
+// interface used for example for orders, shipment, etc.
+// type used to make alias for string, number, etc. to describe function types
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Home',
+    children: [
+      {
+        label: 'News',
+        href: '#',
+      },
+      {
+        label: 'Events',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Projects',
+    children: [
+      {
+        label: 'Current Project',
+        subLabel: 'Frontend with Chakra UI',
+        href: '#',
+      },
+      {
+        label: 'Older Projects',
+        subLabel: 'Full list of our projects, Check It Out!',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'About',
+    href: '#',
+  },
+  {
+    label: 'Contact Us',
+    href: '#',
+  },
+];
